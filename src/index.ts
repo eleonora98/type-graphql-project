45c008d7b3ca51e24feff8c30f7c7556a38b8e02
eleonora98 <eleonora_peneva@abv.bg>
2023-06-11 17:15:21 +0300

    changes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { ApolloServer } from "apollo-server";
 import { buildSchema } from "type-graphql";
-import { UserResolver } from "./resolvers/UserRresolver";
+import { UserResolver } from "./resolvers/user.resolver";
 
 async function main() {
   const connection = await createConnection()
